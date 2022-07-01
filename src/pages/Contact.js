2251fd1f661cdc8bb95ecc07f803932a799bf896
Brooke Love <savienloveb@gmail.com>
2022-06-30 21:58:04 -0700

    creating contact information

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,18 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 export default function Contact() {
-  return <h1>Contact Page</h1>;
+  return (
+    <div>
+      <div>
+        <Navbar />
+        <div>
+          <h1>hiya</h1>
+          <p>If you want to chat feel free to reach out on these links</p>
+          <a>LinkdIn: Brooke Love</a>
+          <a>Github: brookelove</a>
+        </div>
+      </div>
+    </div>
+  );
 }
