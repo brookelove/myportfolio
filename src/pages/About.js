6993def1bcc75cd5e4ece8b0ad0835d69fdb9f84
Brooke Love <savienloveb@gmail.com>
2022-06-30 21:51:26 -0700

    styling a work page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,29 +1,35 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 import "../styles/About.css";
 export default function About() {
   return (
-    <div className="aboutContainer">
-      {/* paragraph text container */}
-      <div className="aboutWords">
-        <h1 className="pageTitle">About</h1>
-        <h3 className="aboutBio">
-          I'm Brooke. A zoologist, web designer, and organized
-        </h3>
-        <h4 className="aboutSubTitle">EXPERIENCE</h4>
-        <h4 className="aboutSubTitle">SKILLS</h4>
-        <button>
-          <a>My Resume</a>
-        </button>
-        <h4 className="aboutSubTitle">SHOUT OUTS</h4>
-        <button>
-          <a>Contact me</a>
-        </button>
-        <button>
-          <a>Blog</a>
-        </button>
+    <div className="beigeBackgd">
+      <div className="aboutContainer">
+        < Navbar />
+        {/* paragraph text container */}
+        <div className="aboutWords">
+          <h1 className="pageTitle">About</h1>
+          <h3 className="aboutBio">
+            I'm Brooke. A zoologist, web designer, and organized
+          </h3>
+          <h4 className="aboutSubTitle">EXPERIENCE</h4>
+          <h4 className="aboutSubTitle">SKILLS</h4>
+          <button>
+            <a>My Resume</a>
+          </button>
+          <h4 className="aboutSubTitle">SHOUT OUTS</h4>
+          <button>
+            <a>Contact me</a>
+          </button>
+          <button>
+            <a>Blog</a>
+          </button>
+        </div>
+        {/* photo container */}
+        <div className="aboutPhoto">
+          {/* image of myself that looks great */}
+        </div>
       </div>
-      {/* photo container */}
-      <div className="aboutPhoto">{/* image of myself that looks great */}</div>
     </div>
   );
 }
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,42 +1,61 @@
 import React from "react";
-
+import Navbar from "../components/Navbar";
+import "../styles/Work.css";
 export default function Work() {
   return (
-    <div>
-      {/* container for the work text */}
-      <div>
-        <h4>Work</h4>
-        {/* this is some of my greatest work that coveres different fields */}
-      </div>
-      {/* container for the project title */}
-      <div>
-        <div>
-          <h1>Project 1</h1>
-          <h5>-front end developer</h5>
+    <div className="darkBkgrnd">
+      <div className="workContainer">
+        <Navbar />
+        <div className="workTextContainer">
+          {/* container for the work text */}
+          <h4 className="pageTitle darkTitle">Work</h4>
+          <br />
+          <p>
+            This is some of my greatest works currently. I have decided to show
+            all of my passions and works that I have done over the year. Some
+            are by resquest but, all are passion projects of mine.
+          </p>
+          {/* this is some of my greatest work that coveres different fields */}
+          <br />
+          <button>
+            <a className="buttonLink" href="/tools">
+              Helpful Tools
+            </a>
+          </button>
         </div>
-        <div>
-          <h1>Project 2</h1>
-          <h5>-front end developer</h5>
+        {/* container for the project title */}
+        <div className="projectContainer">
+          <h1 className="projectTitle">
+            <a href="/project1" className="projectLink">
+              Battle Trail
+            </a>
+          </h1>
+          <h1 className="projectTitle">
+            <a href="/project2" className="projectLink">
+              Evergreen Body Mod
+            </a>
+          </h1>
+          <h1 className="projectTitle">
+            <a href="/project3" className="projectLink">
+              Cake Man
+            </a>
+          </h1>
+          <h1 className="projectTitle">
+            <a href="/project4" className="projectLink">
+              Project 4
+            </a>{" "}
+          </h1>
+          <h1 className="projectTitle">
+            <a href="/project5" className="projectLink">
+              Project 5
+            </a>
+          </h1>
+          <h1 className="projectTitle">
+            <a href="/project6" className="projectLink">
+              Project 6
+            </a>
+          </h1>
         </div>
-        <div>
-          <h1>Project 3</h1>
-          <h5>-front end developer</h5>
-        </div>
-        <div>
-          <h1>Project 4</h1>
-          <h5>-front end developer</h5>
-        </div>
-        <div>
-          <h1>Project 5</h1>
-          <h5>-front end developer</h5>
-        </div>
-        <div>
-          <h1>Project 6</h1>
-          <h5>-front end developer</h5>
-        </div>
-        <button>
-          <a>Helpful Tools</a>
-        </button>
       </div>
     </div>
   );
