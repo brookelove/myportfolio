8603e8ff3e7e1ab0b2f9006cb6079ec367c5dd25
Brooke Love <savienloveb@gmail.com>
2022-07-05 23:17:51 -0700

    creating a home navigation link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,34 +5,59 @@ export default function Navbar() {
   return (
     <div className="nav">
       <div className="innerNav">
-        <p className="navLink">
-          <a
-            className="leftlink"
-            target="_blank"
-            href="https://www.linkedin.com/in/savien-love-898429230/"
-          >
-            LI
-          </a>
-        </p>
-        <p className="navLink ">
-          <a
-            className="leftlink"
-            target="_blank"
-            href="https://github.com/brookelove"
-          >
-            GH
-          </a>
-        </p>
-        <p className="navLink">
-          <a className="leftlink" target="_blank">
-            CP
-          </a>
-        </p>
-        <p className="navLink">
-          <a className="leftlink" target="_blank">
-            SO
-          </a>
-        </p>
+        {document.location.pathname === "/" && (
+          <p className="navLink">
+            <a
+              className="leftlink"
+              target="_blank"
+              href="https://www.linkedin.com/in/savien-love-898429230/"
+            >
+              LI
+            </a>
+          </p>
+        )}
+        {document.location.pathname === "/" && (
+          <p className="navLink">
+            <a
+              className="leftlink"
+              target="_blank"
+              href="https://github.com/brookelove"
+            >
+              GH
+            </a>
+          </p>
+        )}
+        {document.location.pathname === "/" && (
+          <p className="navLink">
+            {/* codepen */}
+            <a className="leftlink" target="_blank">
+              CP
+            </a>
+          </p>
+        )}
+        {document.location.pathname === "/" && (
+          <p className="navLink">
+            {/* stack Overflow */}
+            <a className="leftlink" target="_blank">
+              SO
+            </a>
+          </p>
+        )}
+        {document.location.pathname !== "/" && (
+          <p className="navLink">
+            {/* stack Overflow */}
+            <a
+              className="leftlink"
+              target="_blank"
+              onClick={() => {
+                window.location.href = "/";
+              }}
+            >
+              HOME
+            </a>
+          </p>
+        )}
+
         <span className="line"></span>
       </div>
       <p className="navLink">© 2022</p>
