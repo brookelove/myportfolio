a4914cf4ce46130341abc0b74a394824cbf350aa
Brooke Love <savienloveb@gmail.com>
2022-07-05 22:07:52 -0700

    adding in new fonts

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div className="beigeBackgd">
       <div className="aboutContainer">
-        < Navbar />
+        <Navbar />
         {/* paragraph text container */}
         <div className="aboutWords">
           <h1 className="pageTitle">About</h1>
@@ -13,13 +13,24 @@ export default function About() {
             I'm Brooke. A zoologist, web designer, and organized
           </h3>
           <h4 className="aboutSubTitle">EXPERIENCE</h4>
+          <ul>
+            <li className="aboutContext listElement">Graduated from WSU with a BS of Biology 2021</li>
+            <li className="aboutContext listElement">
+              Graduated from University of Washington with a Certificate of
+              FullStack development 2022
+            </li>
+          </ul>
           <h4 className="aboutSubTitle">SKILLS</h4>
+          <p className="aboutContext"> Object Oriented Design - Organization - Full-stack development - Collaboration - Project Management - CSS - React - JS - Sequilize - MongoDB - Apollo </p>
           <button>
             <a className="buttonLink">My Resume</a>
           </button>
           <h4 className="aboutSubTitle">SHOUT OUTS</h4>
+          <p className="aboutContext">I would like to thank the poeple that have helped me thoughout my journey beginning my career in Fullstack development. I would like to thak my family, who support me though all of my aspirations in my career. I would also like to thank the people who helped me breech the world of tech through working on projects or helped me: <a>Joe Refuss</a>, <a>Lucas Roman</a>, <a>Frantz Felix </a>, <a>James Harding</a>, <a>Dahlia Graves</a>, <a>Evan Hosni</a>,  <a>Lindsay Fitzgerald</a>, <a>Niles Bingham</a></p>
           <button>
-            <a href="/contact" className="buttonLink">Contact me</a>
+            <a href="/contact" className="buttonLink">
+              Contact me
+            </a>
           </button>
           <button>
             <a className="buttonLink">Blog</a>
